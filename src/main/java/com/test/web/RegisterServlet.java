682acf0e83392e1d15da04f8e465848f8d39ee05
zhangrq <2667919698@qq.com>
2022-06-08 11:21:11 +0800

    第一次提交初始化

---
+++ src/main/java/com/test/web/RegisterServlet.java
@@ -0,0 +1,53 @@
+package com.test.web;
+
+import com.test.mapper.UserMapper;
+import com.test.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.InputStream;
+
+@WebServlet("/registerServlet")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User user= new User();
+        user.setPassword(password);
+        user.setUsername(username);
+
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        User u = userMapper.selectByUsername(username);
+
+        response.setContentType("text/html;charset=utf-8");
+        if (u == null){
+            userMapper.add(user);
+            response.getWriter().write("注册成功");
+            sqlSession.commit();
+            sqlSession.close();
+        }else {
+
+            response.getWriter().write("用户名已存在");
+
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
